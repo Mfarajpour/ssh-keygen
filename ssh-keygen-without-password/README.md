b451d2cd81a971ccc667b2fe08ad9e1a04af7b81
mfarajpour <mohsenfarajpour1366@gmail.com>
2023-12-17 17:44:45 +0000

    Auto generate key without password

---
+++ ssh-keygen-without-password/README.md
@@ -0,0 +1,1 @@
+Automating ssh key generation without a password,only enter a name for the key Then The key is made in ssh dir in the path  ~/.ssh .
---
+++ ssh-keygen-without-password/rsa2048.sh
@@ -0,0 +1,18 @@
+echo "ssh key generation without pass "
+echo "No overwrite capability "
+
+echo "enter key name "
+read -p "name : "  name
+
+ls ~/.ssh | grep -x $name 
+
+#echo "ppp" $?
+if [[ $? -eq 1 ]];then
+	ssh-keygen -t rsa -b 2048 -f ~/.ssh/$name -P -N 
+	echo "$name was generated" 
+
+else 
+  echo "this key name is available , please choose another name "	
+fi  
+ 
+
